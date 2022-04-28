f0147cd8082017b4595a54037daf58c1da42aa5f
Teocalazans <teocalazans20@gmail.com>
2022-04-28 17:41:31 -0300

    Estudos do dia 28/04/2022 e conclusão do Iniciante em Programação - F2T2

--- JavaScript/introducao-javascript/js/filtro.js
+++ JavaScript/introducao-javascript/js/filtro.js
@@ -1,20 +1,32 @@
 var campoFiltro = document.querySelector("#filtra-tabela");
 
-campoFiltro.addEventListener("input", function(){
+campoFiltro.addEventListener("input", function () {
     console.log(this.value);
 
     var pacientes = document.querySelectorAll(".paciente");
 
-    for (var i = 0; i < pacientes.length; i++) {
-        var paciente = pacientes[i];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
+    if (this.value.length > 0) {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
 
-        if(nome != this.value) {
-            paciente.classList.add("invisivel")
+            var expressao = new RegExp(this.value, "i")
+
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel")
+            } else {
+                paciente.classList.remove("invisivel");
+            }
         }
+    } else {
+        for (var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel")
 
-        
+        }
     }
+
+
 
 })
--- JavaScript/introducao-javascript/js/form.js
+++ JavaScript/introducao-javascript/js/form.js
@@ -5,7 +5,7 @@ botaoAdicionar.addEventListener("click", function (event) {
 	var form = document.querySelector("#form-adiciona");
 
 	var paciente = obtemPacienteDoFormulario(form);
-	var pacienteTr = montaTr(paciente)
+	
 
 	var erros = validaPaciente(paciente)
 	console.log(erros)
@@ -15,13 +15,19 @@ botaoAdicionar.addEventListener("click", function (event) {
 		return;
 	}
 
-	var tabela = document.querySelector("#tabela-pacientes");
 
-	tabela.appendChild(pacienteTr);
+	adicionaPacienteNaTabela(paciente);
+	
 	form.reset();
 	var mensagemErro = document.querySelector("#mensagens-erros");
 	mensagemErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+	var pacienteTr = montaTr(paciente)
+	var tabela = document.querySelector("#tabela-pacientes");
+	tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagemdeErro(erros) {
 	var ul = document.querySelector("#mensagens-erros")
